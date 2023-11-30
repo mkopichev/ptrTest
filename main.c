1994ea91639dfd6d638b0358624e082dbd6a16f9
mkopichev <mmkopychev@etu.ru>
2023-12-01 01:28:49 +0300

    program finished

--- main.c
+++ main.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
 
-int main(void){
-    
+void valuePass(int i) {
+    i += 1;
+    printf("Var i inside valuePass() has address %p \n", &i);
+}
+
+void ptrPass(int *i) {
+    *i += 1;
+    printf("Var i inside refPass() has address %p \n", i);
+}
+
+int main(void) {
+
+    int i = 10;
+    int *iPtr = &i;
+
+    printf("Var i has initial value %d and address %p \n\n", i, iPtr);
+    valuePass(i);
+    printf("Var i after valuePass() has value %d and address %p \n\n", i, iPtr);
+    ptrPass(iPtr);
+    printf("Var i after ptrPass() has value %d and address %p \n\n", i, iPtr);
+
     return 0;
 }
